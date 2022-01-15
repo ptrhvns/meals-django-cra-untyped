f8920508df7459a18d68db023dfc3e35395388e5
Peter Havens <peter.havens@gmail.com>
2022-01-14 17:00:22 -0700

    Add CSRF cookie support to API; improve signup stub

--- server/main/urls.py
+++ server/main/urls.py
@@ -3,6 +3,6 @@ from django import urls
 from main import views
 
 urlpatterns = [
-    urls.path("csrf_token/", views.csrf_token, name="csrf_token"),
+    urls.path("csrf-token-cookie/", views.csrf_token_cookie, name="csrf_token_cookie"),
     urls.path("signup/", views.signup, name="signup"),
 ]
--- server/main/views.py
+++ server/main/views.py
@@ -1,6 +1,14 @@
-def csrf_token(request):
-    pass  # FIXME
+from django.views.decorators import csrf
+from rest_framework import decorators as rf_decorators
+from rest_framework import response, status
 
 
+@csrf.ensure_csrf_cookie
+@rf_decorators.api_view(http_method_names=["GET"])
+def csrf_token_cookie(request):
+    return response.Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@rf_decorators.api_view(http_method_names=["POST"])
 def signup(request):
-    pass  # FIXME
+    return response.Response({"message": "debug"})

211de890e4504b99db2c336435bfd1123534071b
Peter Havens <peter.havens@gmail.com>
2022-01-20 09:13:22 -0700

    Add djangoresetframework settings

--- server/config/settings.py
+++ server/config/settings.py
@@ -124,3 +124,16 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "main.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication"
+    ],
+    "DEFAULT_RENDERER_CLASSES": ["rest_framework.renderers.JSONRenderer"],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "50/second", "user": "50/second"},
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
+}

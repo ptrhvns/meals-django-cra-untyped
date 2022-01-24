9d7929885f27619a969e466e88fd125922911bac
Peter Havens <peter.havens@gmail.com>
2022-01-24 13:13:08 -0700

    Add tests for csrf_token_cookie view

---
+++ server/main/tests/views/test_csrf_token_cookie.py
@@ -0,0 +1,21 @@
+from django import urls
+from rest_framework import status
+
+from main import views
+
+
+def test_cookie(client):
+    response = client.get(urls.reverse("csrf_token_cookie"))
+    assert len(response.client.cookies["csrftoken"]) > 0
+
+
+def test_http_method_names():
+    assert sorted(views.csrf_token_cookie.view_class.http_method_names) == [
+        "get",
+        "options",
+    ]
+
+
+def test_status(client):
+    response = client.get(urls.reverse("csrf_token_cookie"))
+    assert response.status_code == status.HTTP_204_NO_CONTENT

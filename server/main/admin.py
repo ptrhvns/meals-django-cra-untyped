74f2da5111ff5b9eb29c8a0b44750d2bcdf4876f
Peter Havens <peter.havens@gmail.com>
2022-01-24 11:33:29 -0700

    Register new models with admin site

--- server/main/admin.py
+++ server/main/admin.py
@@ -1,3 +1,13 @@
-#  from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from main import models
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Token)
+class TokenAdmin(admin.ModelAdmin):
+    pass

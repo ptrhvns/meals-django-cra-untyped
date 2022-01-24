1d3d75ce009f4806d8f8f8617f20ad2a6867959c
Peter Havens <peter.havens@gmail.com>
2022-01-24 11:33:51 -0700

    Only allow access to admin site in development environment (for now)

--- server/config/urls.py
+++ server/config/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django import urls
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
     urls.path("api/", urls.include("main.urls")),
-    urls.path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [urls.path("admin/", admin.site.urls)]

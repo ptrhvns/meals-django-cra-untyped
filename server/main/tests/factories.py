9e7c4efc074398479bfed010f1d98f94d442acfb
Peter Havens <peter.havens@gmail.com>
2022-01-25 11:07:59 -0700

    Add test factories

---
+++ server/main/tests/factories.py
@@ -0,0 +1,16 @@
+import factory
+
+from main import models
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.User
+
+    email = factory.LazyAttribute(lambda u: f"{u.username}@example.com")
+    username = factory.Sequence(lambda n: f"user{n}")
+
+
+class TokenFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Token

44a688ed854ff70107c3aaaca00bf4e4208b77ec
Peter Havens <peter.havens@gmail.com>
2022-01-25 16:25:57 -0700

    Refactor send_signup_confirmation task and add tests

--- server/main/tasks.py
+++ server/main/tasks.py
@@ -1,7 +1,8 @@
+import smtplib
+
 import celery
 from celery.utils import log
 from django.conf import settings
-from django.core import mail
 from django.template import loader
 from django.utils.translation import gettext as _
 
@@ -12,6 +13,12 @@ logger = log.get_logger(__name__)
 
 @celery.shared_task
 def send_signup_confirmation(user_id, site_uri, confirmation_uri):
+    _send_signup_confirmation(user_id, site_uri, confirmation_uri)
+
+
+def _send_signup_confirmation(user_id, site_uri, confirmation_uri):
+    logger.info(f"attempting to send signup confirmation email to user ID {user_id}")
+
     context = {
         "confirmation_uri": confirmation_uri,
         "site_title": settings.SITE_TITLE,
@@ -26,17 +33,24 @@ def send_signup_confirmation(user_id, site_uri, confirmation_uri):
         "main/email/signup_confirmation.html", context=context
     )
 
-    recipient_list = [models.User.objects.get(pk=user_id).email]
+    try:
+        user = models.User.objects.get(pk=user_id)
+    except models.User.DoesNotExist:
+        logger.error(f"user ID {user_id} does not exist in database")
+        return
 
-    logger.info(f"sending email confirmation to user with ID {user_id}")
+    try:
+        user.email_user(
+            _(f"Signup confirmation for {settings.SITE_TITLE}."),
+            message,
+            fail_silently=False,
+            from_email=settings.EMAIL_ADDRESSES["support"],
+            html_message=html_message,
+        )
+    except smtplib.SMTPException:
+        logger.error(f"failed to deliver email to user ID {user.id}")
+        return
 
-    mail.send_mail(
-        fail_silently=False,
-        from_email=settings.EMAIL_ADDRESSES["support"],
-        html_message=html_message,
-        message=message,
-        recipient_list=recipient_list,
-        subject=_(f"Signup confirmation for {settings.SITE_TITLE}."),
+    logger.info(
+        f"successfully sent signup confirmation email to user with ID {user.id}"
     )
-
-    logger.info(f"email confirmation sent successfully to user with ID {user_id}")
---
+++ server/main/tests/tasks/test_send_signup_confirmation.py
@@ -0,0 +1,56 @@
+import smtplib
+
+import pytest
+from django.conf import settings
+
+from main import models, tasks
+from main.tests import factories
+
+user_id = 777
+site_uri = "http://example.com/"
+confirmation_uri = f"{site_uri}/signup-confirmation/exampletoken/"
+
+
+def test_task_dispatches_logic(mocker):
+    mock = mocker.patch("main.tasks._send_signup_confirmation")
+    tasks.send_signup_confirmation(user_id, site_uri, confirmation_uri)
+    mock.assert_called_with(user_id, site_uri, confirmation_uri)
+
+
+@pytest.mark.django_db
+def test_emailing_user(mocker):
+    mock = mocker.patch("main.models.User.email_user")
+    user = factories.UserFactory.create()
+    tasks._send_signup_confirmation(user.id, site_uri, confirmation_uri)
+    assert mock.called
+    assert isinstance(mock.call_args.args[0], str)
+    assert len(mock.call_args.args[0]) > 0
+    assert isinstance(mock.call_args.args[1], str)
+    assert len(mock.call_args.args[1]) > 0
+    assert mock.call_args.kwargs["fail_silently"] is False
+    assert mock.call_args.kwargs["from_email"] == settings.EMAIL_ADDRESSES["support"]
+    assert isinstance(mock.call_args.kwargs["html_message"], str)
+    assert len(mock.call_args.kwargs["html_message"]) > 0
+
+
+@pytest.mark.django_db
+def test_user_does_not_exist(mocker):
+    get_mock = mocker.patch("main.models.User.objects.get")
+    get_mock.side_effect = models.User.DoesNotExist()
+    error_mock = mocker.patch("main.tasks.logger.error")
+    tasks._send_signup_confirmation(user_id, site_uri, confirmation_uri)
+    assert get_mock.called
+    assert error_mock.called
+    assert len(error_mock.call_args.args[0]) > 0
+
+
+@pytest.mark.django_db
+def test_email_delivery_fails(mocker):
+    email_user_mock = mocker.patch("main.models.User.email_user")
+    email_user_mock.side_effect = smtplib.SMTPException()
+    error_mock = mocker.patch("main.tasks.logger.error")
+    user = factories.UserFactory.create()
+    tasks._send_signup_confirmation(user.id, site_uri, confirmation_uri)
+    assert email_user_mock.called
+    assert error_mock.called
+    assert len(error_mock.call_args.args[0]) > 0

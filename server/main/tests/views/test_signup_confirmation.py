a0a0c896f1f6840564feba395c002f85d194fd68
Peter Havens <peter.havens@gmail.com>
2022-01-25 11:08:16 -0700

    Add tests for signup_confirmation view

---
+++ server/main/tests/views/test_signup_confirmation.py
@@ -0,0 +1,68 @@
+import datetime
+
+import pytest
+from django import urls
+from django.utils import timezone
+from rest_framework import status
+
+from main import models, views
+from main.tests import factories
+
+
+def test_allowed_http_method_names():
+    assert sorted(views.signup_confirmation.view_class.http_method_names) == [
+        "options",
+        "post",
+    ]
+
+
+def test_confirming_signup_with_invalid_data(client):
+    response = client.post(urls.reverse("signup_confirmation"), {})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    json = response.json()
+    assert len(json["errors"]) > 0
+    assert len(json["message"]) > 0
+
+
+@pytest.mark.django_db
+def test_confirming_signup(client):
+    user = factories.UserFactory.create()
+
+    token = factories.TokenFactory.create(
+        category=models.Token.EMAIL_CONFIRMATION,
+        expiration=timezone.now() + datetime.timedelta(hours=24),
+        user=user,
+    )
+
+    response = client.post(urls.reverse("signup_confirmation"), {"token": token.token})
+    assert response.status_code == status.HTTP_200_OK
+    json = response.json()
+    assert len(json["message"]) > 0
+    assert not models.Token.objects.filter(token=token.token).exists()
+    user.refresh_from_db()
+    assert user.is_active is True
+
+
+@pytest.mark.django_db
+def test_confirming_signup_with_non_existant_token(client):
+    response = client.post(urls.reverse("signup_confirmation"), {"token": "invalid"})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert len(response.json()["message"]) > 0
+
+
+@pytest.mark.django_db
+def test_confirming_signup_with_expired_token(client):
+    user = factories.UserFactory.create()
+    expiration_in_the_past = timezone.now() - datetime.timedelta(hours=1)
+
+    token = factories.TokenFactory.create(
+        category=models.Token.EMAIL_CONFIRMATION,
+        expiration=expiration_in_the_past,
+        user=user,
+    )
+
+    response = client.post(urls.reverse("signup_confirmation"), {"token": token.token})
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert len(response.json()["message"]) > 0
+    assert not models.Token.objects.filter(token=token.token).exists()

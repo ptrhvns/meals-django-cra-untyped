1ecc095a06b9eecf0a8ada2b63a13fd8e9983825
Peter Havens <peter.havens@gmail.com>
2022-01-29 11:04:55 -0700

    Add tests for NotFound

---
+++ client/src/routes/NotFound.test.js
@@ -0,0 +1,25 @@
+import NotFound from "./NotFound";
+import ReactDOM from "react-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { MemoryRouter } from "react-router-dom";
+import { render, waitFor } from "@testing-library/react";
+
+function buildComponent() {
+  return (
+    <MemoryRouter>
+      <HelmetProvider>
+        <NotFound />
+      </HelmetProvider>
+    </MemoryRouter>
+  );
+}
+
+it("renders successfully", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(buildComponent(), div);
+});
+
+it("renders the correct <title>", async () => {
+  const component = render(buildComponent());
+  await waitFor(() => expect(document.title).toContain("Not Found"));
+});

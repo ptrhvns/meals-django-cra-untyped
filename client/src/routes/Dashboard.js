259307a9c31bb07e9dd7fb42dbc735adf2f23e5b
Peter Havens <peter.havens@gmail.com>
2022-02-08 16:36:16 -0700

    Add initial layout

--- client/src/routes/Dashboard.js
+++ client/src/routes/Dashboard.js
@@ -1,5 +1,24 @@
+import Container from "../components/Container";
+import Navbar from "../components/Navbar";
+import { buildTitle } from "../lib/utils";
+import { Helmet } from "react-helmet-async";
+
 function Dashboard() {
-  return <div className="dashboard">Dashboard</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>{buildTitle("Dashboard")}</title>
+      </Helmet>
+
+      <Navbar />
+
+      <Container variant="viewport">
+        <Container variant="content">
+          <div>Dashboard</div>
+        </Container>
+      </Container>
+    </div>
+  );
 }
 
 export default Dashboard;
--- client/src/routes/Dashboard.test.js
+++ client/src/routes/Dashboard.test.js
@@ -1,11 +1,25 @@
 import Dashboard from "./Dashboard";
 import ReactDOM from "react-dom";
+import { render, waitFor } from "@testing-library/react";
+import { HelmetProvider } from "react-helmet-async";
+import { MemoryRouter } from "react-router-dom";
 
 function buildComponent() {
-  return <Dashboard />;
+  return (
+    <MemoryRouter>
+      <HelmetProvider>
+        <Dashboard />
+      </HelmetProvider>
+    </MemoryRouter>
+  );
 }
 
 it("renders successfully", () => {
   const div = document.createElement("div");
   ReactDOM.render(buildComponent(), div);
+});
+
+it("renders the correct <title>", async () => {
+  const component = render(buildComponent());
+  await waitFor(() => expect(document.title).toContain("Dashboard"));
 });

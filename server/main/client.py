d9dbc2dcf6698b391662605d617ad7bf257151f1
Peter Havens <peter.havens@gmail.com>
2022-04-08 06:39:06 -0600

    Add comment

--- server/main/client.py
+++ server/main/client.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 
+# These represent URLs or routes available on the client app.
 urls = {
     "home": settings.BASE_CLIENT_URI,
     "signup_confirmation": settings.BASE_CLIENT_URI + "/signup-confirmation/{token}",

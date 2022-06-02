5d3011b5056aba3137a48f2306d266ca67d1c96f
Peter Havens <peter.havens@gmail.com>
2022-06-02 11:31:53 -0600

    Add comment

--- web/src/hooks/useOutsideClick.js
+++ web/src/hooks/useOutsideClick.js
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from "react";
 
+// Apply returned ref to an element. If user clicks outside of that element, the
+// the callback will be called.
 export default function useOutsideClick(callback) {
   const ref = useRef();
 
